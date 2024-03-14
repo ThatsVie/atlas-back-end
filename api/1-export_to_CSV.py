44852569ee2f305e076b9a4551d0e1911eb61bea
ThatsVie <viepaulalmt@gmail.com>
2024-03-14 08:23:48 -0500

    extend Python script to export data in the CSV format.

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""
+- Script to Retrieve Employee TODO List Progress
+- Utilizes either the urllib or requests module to interact with the REST API
+- Accepts an integer as a parameter, representing the employee ID.
+-Output Format: The script must display the employee's TODO list progress
+in the following exact format:
+- The first line should indicate the employee's name and their progress
+with tasks, represented as (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+- EMPLOYEE_NAME: Name of the employee.
+- NUMBER_OF_DONE_TASKS: Number of completed tasks.
+- TOTAL_NUMBER_OF_TASKS: Total number of tasks, including both
+completed and non-completed tasks.
+- The subsequent lines should display the titles of completed tasks,
+with one tabulation and one space before each task title.
+-Extended to export data in the CSV format.
+-Records all tasks that are owned by this employee
+- Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+- File name must be: USER_ID.csv
+"""
+import csv
+import requests
+import sys
+
+
+BASE_URL = "https://jsonplaceholder.typicode.com/users"
+
+
+def retrieve_employee_name(employee_id):
+    """
+    Retrieves the name of the employee.
+    """
+    url = f"{BASE_URL}/{employee_id}"
+    response = requests.get(url)
+    return response.json().get("name")
+
+
+def retrieve_assigned_tasks_count(employee_id):
+    """
+    Retrieves the total number of tasks assigned to the employee.
+    """
+    url = f"{BASE_URL}/{employee_id}/todos"
+    response = requests.get(url)
+    return len(response.json())
+
+
+def retrieve_completed_tasks(employee_id):
+    """
+    Retrieves the list of tasks completed by the employee.
+    """
+    completed_tasks = []
+    url = f"{BASE_URL}/{employee_id}/todos"
+    response = requests.get(url)
+    for task in response.json():
+        if task.get("completed"):
+            completed_tasks.append(task)
+    return completed_tasks
+
+
+def print_employee_progress(employee_name, completed_tasks, assigned_tasks):
+    """
+    Prints the employee's task list progress.
+    """
+    print(
+        f"Employee {employee_name} is done with tasks("
+        f"{len(completed_tasks)}/{assigned_tasks}):"
+    )
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+    return completed_tasks
+
+
+def export_to_csv(employee_id, employee_name, completed_tasks):
+    """
+    Exports completed tasks to a CSV file.
+    """
+    file_name = f"{employee_id}.csv"
+    with open(file_name, "w", newline="") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for task in completed_tasks:
+            writer.writerow({
+                "USER_ID": employee_id,
+                "USERNAME": employee_name,
+                "TASK_COMPLETED_STATUS": task["completed"],
+                "TASK_TITLE": task["title"]
+            })
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    employee_name = retrieve_employee_name(employee_id)
+    assigned_tasks = retrieve_assigned_tasks_count(employee_id)
+    completed_tasks = retrieve_completed_tasks(employee_id)
+    completed_tasks = print_employee_progress(employee_name, completed_tasks, assigned_tasks)
+    export_to_csv(employee_id, employee_name, completed_tasks)
